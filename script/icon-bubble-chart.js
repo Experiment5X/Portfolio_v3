419ec102ba7d7898c567a4d9b59e4ac724fc4a25
Adam Spindler <xmeadamx@gmail.com>
2020-03-08 15:42:23 -0700

    Parameterize icon bubble

--- script/icon-bubble-chart.js
+++ script/icon-bubble-chart.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
-    const width = 300;
-    const height = 300;
+    const size = Math.min(window.innerHeight, window.innerWidth) * 0.4;
+    const width = size;
+    const height = size;
 
     // setup the chart svg
     const svg = d3.select('.icon-bubble')
@@ -10,78 +11,60 @@ $(document).ready(() => {
         .append('g')
         .attr('transform', 'translate(0, 0)');
 
+    const calcRadiusScale = () => d3.scaleSqrt().domain([20, 100]).range([size / 35 * 1.25, size / 35 * 3]);
+
     const simulation = d3.forceSimulation()
         .force('x', d3.forceX(width / 2).strength(0.05))
         .force('y', d3.forceY(height / 2).strength(0.05))
-        .force('collide', d3.forceCollide(d => radiusScale(Math.sqrt(2) * d.score)))
-
-    
-    const radiusScale = d3.scaleSqrt().domain([20, 100]).range([10, 25])
-
-    const onBubbleClick = (d) => {
-        d.score *= 2;
-        console.log('on click', d);
-        simulation.force('hover-force', d3.forceCenter(d.x, d.y));
-    };
-
-    const onBubbleHover = (d) => {
-        simulation.force('hover-force', d3.forceCenter(d.x, d.y));
-        console.log('on hover');
-    };
+        .force('collide', d3.forceCollide(d => calcRadiusScale()(Math.sqrt(2) * d.score)));
 
     // load the data and draw the data points
     d3.csv('/script/data.csv').then(dataPoints => {
-        const circles = svg.selectAll('language')
-            .data(dataPoints)
-            .enter().append('image')
-            .attr('href', d => `/images/tech-icons/${d.image}`)
-            .attr('class', 'language')
-            .attr('width', d => 2 * radiusScale(d.score) * Math.sqrt(1 / 2))
-            .attr('height', d => 2 * radiusScale(d.score) * Math.sqrt(1 / 2))
-            .on('click', (d) => {
-                console.log('clicked ', d.image);
-                const transformedData = dataPoints.map(dp => {
-                    // deselect all the other ones
-                    if (dp.doubled) {
-                        return {
-                            ...dp,
-                            doubled: undefined,
-                            score: dp.score / 2,
-                        }
-                    } else {
-                        return dp
-                    }
-                }).map(dp => {
-                    // check if image clicked on
-                    if (dp.image === d.image) {
-                        return {
-                            ...dp,
-                            doubled: true,
-                            score: dp.score * 2,
-                        }
-                    }
-                    else {
-                        return dp
-                    }
-                } )
-                simulation.force('x').initialize(transformedData);
-                simulation.force('y').initialize(transformedData);
-                simulation.force('collide').initialize(transformedData);
-            })
-            .on('hover', onBubbleHover);
+        const updateData = (data) => {
+            const circles = svg.selectAll('.language')
+                .data(data)
+                .enter().append('image')
+                .attr('href', d => `/images/tech-icons/${d.image}`)
+                .attr('class', 'language')
+                .attr('width', d => 2 * calcRadiusScale()(d.score) * Math.sqrt(1 / 2))
+                .attr('height', d => 2 * calcRadiusScale()(d.score) * Math.sqrt(1 / 2))
+                .on('click', (d) => languageClicked(d))
 
-        setTimeout(() => {
-            const randStrength = Math.random() * 0.05;
-            const middleScaleFactor = 0.5 + Math.random() * (0.2 - 0.1);
+            // update the circle positions when the simulation changes their positions
+            simulation.nodes(dataPoints).on('tick', () => {
+                circles.attr('transform', d => `translate(${d.x}, ${d.y})`)
+            });
+        };
 
-            //simulation.force('x').initialize(dataPoints)
-            //simulation.force('y').initialize(dataPoints)
-        }, 100);
+        const languageClicked = (d) => {
+            const updatedDataPoints = dataPoints.map(dp => {
+                if (dp.doubled) {
+                    return {
+                        ...dp,
+                        doubled: undefined,
+                        score: String(Number(dp.score) / 2)
+                    }
+                } else {
+                    return dp
+                }
+            }).map(dp => {
+                if (dp.image === d.image) {
+                    return {
+                        ...dp,
+                        doubled: true,
+                        score: String(dp.score * 2)
+                    }
+                } else {
+                    return dp
+                }
+            });
 
-        // update the circle positions when the simulation changes their positions
-        simulation.nodes(dataPoints).on('tick', () => {
-            circles.attr('transform', d => `translate(${d.x}, ${d.y})`)
-        })
+            console.log(updatedDataPoints);
+            updateData(updatedDataPoints);
+            console.log('Clicked', d);
+        }
+
+        updateData(dataPoints);
     });
 
 });
